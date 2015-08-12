cfb0cf3792f155641830479fcdfb978b05059bbc
Shane Stephens <shane.stephens@gmail.com>
2015-08-11 20:28:51 -0700

    Capture and process phases operational

--- style-phases.js
+++ style-phases.js
@@ -20,7 +20,7 @@ module.exports.amalgamate = phase({input: types.number, output: types.string, ar
         this.names.push(name);
       }
       this.data[this.order[name]] = data;
-      this.id = tags.read('ejsFabricator');
+      this.id = tags.read(this.options.idTag);
     },
     onCompletion: function() {
       var r = '';
@@ -31,6 +31,40 @@ module.exports.amalgamate = phase({input: types.number, output: types.string, ar
       r += this.id + ', ' + this.data.reduce(function(a, b) { return a + ', ' + b});
       return r;
     }
+  },
+  {idTag: ''});
+
+function typeVar(s) { return (function(v) {
+  if (!v[s]) {
+    v[s] = types.newTypeVar();
+  }
+  return v[s];
+}); }
+
+module.exports.mergeTags = phase({input: typeVar('a'), output: typeVar('a'), arity: '1:1'},
+  function(data, tags) {
+    var inputString = this.options.inputs.map(function(input) { return tags.read(input); }).reduce(
+      function(a, b) { return a + '|' + b; });
+    var reString = this.options.inputs.map(function(input) { return "([^\|]*)"}).reduce(
+      function(a, b) { return a + '\\|' + b; });
+    tags.tag(this.options.output, inputString.replace(new RegExp(reString), this.options.spec));
+    return data;
+  },
+  { inputs: [], spec: '', output: 'result' });
+
+module.exports.dateTag = phase({input: typeVar('a'), output: typeVar('a'), arity: '1:1'},
+  function(data, tags) {
+    tags.tag('date', Date.now());
+    return data;
   });
 
+module.exports.hostnameTag = phase({input: typeVar('a'), output: typeVar('a'), arity: '1:1'},
+  function(data, tags) {
+    var os=require('os');
+    var name = os.hostname();
+    if (os.networkInterfaces().eth0 !== undefined)
+      name += ' (' + os.networkInterfaces().eth0[0].address + ')';
+    tags.tag('hostname', name);
+    return data;
+  });
 
